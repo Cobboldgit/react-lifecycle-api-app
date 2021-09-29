a6348e281db7aca54abb31a96ed7f1e47d0a9226
Cobboldgit <86422221+Cobboldgit@users.noreply.github.com>
2021-09-29 13:14:39 +0000

    import and render <UserFunction/>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import UsersClass from './component/UsersClass'
+import UsersFunction from './component/UsersFunction';
 
 function App() {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
+      <h1>Class Component</h1>
       <UsersClass/>
+      <hr/>
+      <hr/>
+      <br/>
+      <hr/>
+      <h1 style={{marginTop: '50px'}}>Function Component</h1>
+      <UsersFunction/>
     </div>
   );
 }
